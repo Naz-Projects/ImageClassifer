d29ccbbc8a76af54c0e8acd0ca4adab760e547ff
Nazmul <nazmul@udel.edu>
2025-05-26 21:55:43 -0400

    finished the prediction model

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import streamlit as st
+ 
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+
+from PIL import Image
+
+def load_model():
+    model = MobileNetV2(weights="imagenet")
+    return model
+
+
+def preprocess_image(image):
+    img = np.array(image)
+    img = cv2.resize(img, (224, 224))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def classify_image(model, image):
+    try:
+        processed_image = preprocess_image(image)
+        predictions = model.predict(preprocess_image)
+        decode_predictions = decode_predictions(predictions, top=3)[0]
+        return decode_predictions
+    
+    except exception as e :
+        st.error(f"Error classifying image: {str(e)}")
+        return None
+
+
